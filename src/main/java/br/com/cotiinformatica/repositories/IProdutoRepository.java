997d6c52a88e22110f5543e15bad557f496c8964
sistemajbst <jbengenhariadeseguranca@gmail.com>
2023-08-24 12:45:20 -0300

    First Commit

---
+++ src/main/java/br/com/cotiinformatica/repositories/IProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.cotiinformatica.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.cotiinformatica.entities.Produto;
+
+public interface IProdutoRepository extends JpaRepository<Produto, UUID> {
+}
